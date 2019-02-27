271b35d2b17c0ab74bc03289c2b7eee4564dac9d
develop <ikhlas.firlana@gmail.com>
2019-02-27 15:44:48 +0700

    no message

---
+++ basic/max_number.js
@@ -0,0 +1,7 @@
+console.log("MIN_SAFE_INTEGER",Number.MIN_SAFE_INTEGER);
+console.log("MAX_SAFE_INTEGER",Number.MAX_SAFE_INTEGER);
+console.log("MIN_VALUE",Number.MIN_VALUE);
+console.log("MAX_VALUE",Number.MAX_VALUE);
+console.log("POSITIVE_INFINITY",Number.POSITIVE_INFINITY);
+console.log("NEGATIVE_INFINITY",Number.NEGATIVE_INFINITY);
+console.log("NaN",Number.NaN);
